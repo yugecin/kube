f4f08c6eea9379b9a1ae29488c866e3b13267445
yugecin <yugecin@users.noreply.github.com>
2022-10-20 17:49:25 +0200

    shadertoy compat

--- frag.glsl
+++ frag.glsl
@@ -1,22 +1,23 @@
 // vi: syntax=c
 #version 430
+#define shadertoy 0
+#if shadertoy == 0
 #define iTime fpar[0].x
 #define debugmov 1 //noexport
+layout (location=0) uniform vec4 fpar[2];
+layout (location=2) uniform vec4 debug[2]; //noexport
+#endif
 #define PI 3.14159265359
 #define HALFPI 1.5707963268
 #define fight(a,b) if(b.x<a.x)a=b; // union of 2 distance function resuls where x is distance and y is material id
-#define ___ 0
-#define RED 1
-#define BLU 2
-#define GRN 3
-#define YLW 4
-#define WHI 5
-#define ORG 6
-#define SHA 7
-layout (location=0) uniform vec4 fpar[2];
-layout (location=2) uniform vec4 debug[2]; //noexport
-out vec4 c;
-in vec2 v;
+#define _x_ 0. /*'___' : macro name with a double underscore is reserved - unintented behavior is possible*/
+#define RED 1.
+#define BLU 2.
+#define GRN 3.
+#define YLW 4.
+#define WHI 5.
+#define ORG 6.
+#define SHA 7.
 int i;
 
 float _atan2(float y, float x)
@@ -31,12 +32,12 @@ float rand(vec2 p){return fract(sin(dot(p.xy,vec2(12.9898,78.233)))*43758.5453);
 
 mat2 rot2(float a){float s=sin(a),c=cos(a);return mat2(c,s,-s,c);}
 
-float ROUNDING = 2.5;
-float SIDE = 12 - ROUNDING;
-float UNIT = SIDE * 2 + 2 * ROUNDING;
-float SPACING = UNIT;
-vec3 offs = vec3(0., SPACING, -SPACING);
-vec4 rot = vec4(0, HALFPI, -HALFPI, PI);
+const float ROUNDING = 2.5;
+const float SIDE = 12. - ROUNDING;
+const float UNIT = SIDE * 2. + 2. * ROUNDING;
+const float SPACING = UNIT;
+const vec3 offs = vec3(0, SPACING, -SPACING);
+const vec4 rot = vec4(0, HALFPI, -HALFPI, PI);
 
 float su(float d1, float d2, float k)
 {
@@ -46,13 +47,13 @@ float su(float d1, float d2, float k)
 vec3 hitPosition = vec3(0);
 
 struct Cube {
-	int col1, col2, col3;
+	float col1, col2, col3;
 	vec3 offset;
 	vec3 rotation;
 };
 Cube cubes[26];
 
-vec2 oneSidedCube(vec3 p, int materialTop)
+vec2 oneSidedCube(vec3 p, float materialTop)
 {
 	vec2	mc = vec2(length(max(abs(p) - SIDE, 0.)) - ROUNDING, 0),
 		tc = vec2(length(max(abs(p + vec3(0., 0., ROUNDING + .02)) - SIDE, 0.)), materialTop);
@@ -60,7 +61,7 @@ vec2 oneSidedCube(vec3 p, int materialTop)
 	return mc.x < tc.x ? mc : tc;
 }
 
-vec2 twoSidedCube(vec3 p, int materialTop, int materialFront)
+vec2 twoSidedCube(vec3 p, float materialTop, float materialFront)
 {
 	vec2	mc = oneSidedCube(p, materialTop),
 		fc = vec2(length(max(abs(p + vec3(0, -ROUNDING - .02, 0.)) - SIDE, 0.)), materialFront);
@@ -69,7 +70,7 @@ vec2 twoSidedCube(vec3 p, int materialTop, int materialFront)
 }
 
 // also includes the shaft
-vec2 centerCube(vec3 p, int materialTop, vec3 pos, vec3 rot)
+vec2 centerCube(vec3 p, float materialTop, vec3 pos, vec3 rot)
 {
 	p -= pos;
 	p.xy *= rot2(rot.x);
@@ -82,7 +83,7 @@ vec2 centerCube(vec3 p, int materialTop, vec3 pos, vec3 rot)
 	return mc.x < sh.x ? mc : sh;
 }
 
-vec2 middleCube(vec3 p, int materialTop, int materialFront, vec3 pos, vec3 rot)
+vec2 middleCube(vec3 p, float materialTop, float materialFront, vec3 pos, vec3 rot)
 {
 	p -= pos;
 	p.xy *= rot2(rot.x);
@@ -94,7 +95,7 @@ vec2 middleCube(vec3 p, int materialTop, int materialFront, vec3 pos, vec3 rot)
 	return res;
 }
 
-vec2 cornerCube(vec3 p, int materialTop, int materialFront, int materialSide, vec3 pos, vec3 rot)
+vec2 cornerCube(vec3 p, float materialTop, float materialFront, float materialSide, vec3 pos, vec3 rot)
 {
 	p -= pos;
 	p.xy *= rot2(rot.x);
@@ -117,6 +118,8 @@ vec2 map(vec3 p)
 		Cube c = cubes[i];
 		vec2 cub;
 		vec3 pa = p;
+		vec3 of = c.offset;
+		/*
 		if (c.offset.x == offs.z) {
 			pa.yz *= rot2(iTime);
 		}
@@ -129,12 +132,20 @@ vec2 map(vec3 p)
 		if (c.offset.y == offs.z) {
 			//pa.xz *= rot2(iTime);
 		}
-		if (c.col2 == ___) {
-			cub = centerCube(pa, c.col1, c.offset, c.rotation);
-		} else if (c.col3 == ___) {
-			cub = middleCube(pa, c.col1, c.col2, c.offset, c.rotation);
+		if (
+			!(c.offset.x == 0 && c.offset.y == 0) &&
+			!(c.offset.x == 0 && c.offset.z == 0) &&
+			!(c.offset.y == 0 && c.offset.z == 0)
+		) {
+			of *= sin(iTime) + 2;
+		}
+		*/
+		if (c.col2 == _x_) {
+			cub = centerCube(pa, c.col1, of, c.rotation);
+		} else if (c.col3 == _x_) {
+			cub = middleCube(pa, c.col1, c.col2, of, c.rotation);
 		} else {
-			cub = cornerCube(pa, c.col1, c.col2, c.col3, c.offset, c.rotation);
+			cub = cornerCube(pa, c.col1, c.col2, c.col3, of, c.rotation);
 		}
 		if (cub.x < res.x) {
 			res = cub;
@@ -153,7 +164,7 @@ vec3 norm(vec3 p, float dist_to_p)
 vec4 march(vec3 ro, vec3 rd, int maxSteps)
 {
 	vec4 r = vec4(0);
-	for (i = 0; i < maxSteps && r.z < 350; i++){
+	for (i = 0; i < maxSteps && r.z < 350.; i++){
 		hitPosition = ro + rd * r.z;
 
 		//p.y += 100.;
@@ -166,7 +177,7 @@ vec4 march(vec3 ro, vec3 rd, int maxSteps)
 		vec2 m = map(hitPosition);
 		float distance = m.x;
 		if (distance < .03) {
-			r.x = 1;
+			r.x = 1.;
 			r.y = distance;
 			r.w = m.y;
 			break;
@@ -176,38 +187,44 @@ vec4 march(vec3 ro, vec3 rd, int maxSteps)
 	return r;
 }
 
+#if shadertoy == 1
+void mainImage(out vec4 c, in vec2 v)
+#else
+out vec4 c;
+in vec2 v;
 void main()
+#endif
 {
 	cubes[ 0] = Cube(RED, YLW, BLU, offs.zyz, rot.yxx),
-	cubes[ 1] = Cube(RED, BLU, ___, offs.xyz, rot.xxx),
+	cubes[ 1] = Cube(RED, BLU, _x_, offs.xyz, rot.xxx),
 	cubes[ 2] = Cube(RED, BLU, WHI, offs.yyz, rot.xxx),
-	cubes[ 3] = Cube(RED, YLW, ___, offs.zxz, rot.yxx),
-	cubes[ 4] = Cube(RED, ___, ___, offs.xxz, rot.xxx),
-	cubes[ 5] = Cube(RED, WHI, ___, offs.yxz, rot.zxx),
+	cubes[ 3] = Cube(RED, YLW, _x_, offs.zxz, rot.yxx),
+	cubes[ 4] = Cube(RED, _x_, _x_, offs.xxz, rot.xxx),
+	cubes[ 5] = Cube(RED, WHI, _x_, offs.yxz, rot.zxx),
 	cubes[ 6] = Cube(RED, GRN, YLW, offs.zzz, rot.wxx),
-	cubes[ 7] = Cube(RED, GRN, ___, offs.xzz, rot.wxx),
+	cubes[ 7] = Cube(RED, GRN, _x_, offs.xzz, rot.wxx),
 	cubes[ 8] = Cube(RED, WHI, GRN, offs.yzz, rot.zxx),
-	cubes[ 9] = Cube(YLW, BLU, ___, offs.zyx, rot.xxz),
-	cubes[10] = Cube(BLU, ___, ___, offs.xyx, rot.xyx),
-	cubes[11] = Cube(BLU, WHI, ___, offs.yyx, rot.zxz),
-	cubes[12] = Cube(YLW, ___, ___, offs.zxx, rot.yyx),
-	cubes[13] = Cube(WHI, ___, ___, offs.yxx, rot.zyx),
-	cubes[14] = Cube(GRN, YLW, ___, offs.zzx, rot.yxz),
-	cubes[15] = Cube(GRN, ___, ___, offs.xzx, rot.xzx),
-	cubes[16] = Cube(WHI, GRN, ___, offs.yzx, rot.wxz),
+	cubes[ 9] = Cube(YLW, BLU, _x_, offs.zyx, rot.xxz),
+	cubes[10] = Cube(BLU, _x_, _x_, offs.xyx, rot.xyx),
+	cubes[11] = Cube(BLU, WHI, _x_, offs.yyx, rot.zxz),
+	cubes[12] = Cube(YLW, _x_, _x_, offs.zxx, rot.yyx),
+	cubes[13] = Cube(WHI, _x_, _x_, offs.yxx, rot.zyx),
+	cubes[14] = Cube(GRN, YLW, _x_, offs.zzx, rot.yxz),
+	cubes[15] = Cube(GRN, _x_, _x_, offs.xzx, rot.xzx),
+	cubes[16] = Cube(WHI, GRN, _x_, offs.yzx, rot.wxz),
 	cubes[17] = Cube(YLW, ORG, BLU, offs.zyy, rot.yyx),
-	cubes[18] = Cube(BLU, ORG, ___, offs.xyy, rot.xyx),
+	cubes[18] = Cube(BLU, ORG, _x_, offs.xyy, rot.xyx),
 	cubes[19] = Cube(BLU, ORG, WHI, offs.yyy, rot.xyx),
-	cubes[20] = Cube(YLW, ORG, ___, offs.zxy, rot.yyx),
-	cubes[21] = Cube(ORG, ___, ___, offs.xxy, rot.xwx),
-	cubes[22] = Cube(WHI, ORG, ___, offs.yxy, rot.zyx),
+	cubes[20] = Cube(YLW, ORG, _x_, offs.zxy, rot.yyx),
+	cubes[21] = Cube(ORG, _x_, _x_, offs.xxy, rot.xwx),
+	cubes[22] = Cube(WHI, ORG, _x_, offs.yxy, rot.zyx),
 	cubes[23] = Cube(GRN, ORG, YLW, offs.zzy, rot.wyx),
-	cubes[24] = Cube(ORG, GRN, ___, offs.xzy, rot.xwx),
+	cubes[24] = Cube(ORG, GRN, _x_, offs.xzy, rot.xwx),
 	cubes[25] = Cube(ORG, GRN, WHI, offs.yzy, rot.xwx);
 
 	vec2 uv=v;uv.y/=1.77;
 
-	vec3 ro = vec3(10 * sin(iTime), -30 * cos(iTime), -20);
+	vec3 ro;
 	vec3 at = vec3(0, 0, -25);
 	//vec3 ro = vec3(20, -50, -20);
 	//ro.x = fpar[0].y/10.;
@@ -237,10 +254,10 @@ void main()
 
 	vec4 result = march(ro, rd, 300);
 
-	if (result.x > 0) { // hit
+	if (result.x > 0.) { // hit
 		vec3 shade = vec3(0);
 		float material = result.w;
-		if (material == 0) {
+		if (material == _x_) {
 			shade = vec3(.03);
 		} else if (material == RED) {
 			shade = vec3(1, 0, 0);
